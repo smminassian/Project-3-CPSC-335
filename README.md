403465ed69f42b44b870f703a7a6f7ad8ce400f8
smminassian <143156014+smminassian@users.noreply.github.com>
2025-04-20 12:13:54 -0700

    made some progress and starting to get logic down

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Project-3-CPSC-335
+References: 
--- wine.cpp
+++ wine.cpp
@@ -1,14 +1,34 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-#include <vector>
-#include <wine.h>
 
-Wine::Wine(vector<int> values, int days) {
-    this->values = resize(days);
-    this->days = days;
-    
+
+int Wine(int *values, int days){
+    int max = NULL;
+    int noSell = values[0];
+    int noBottle = values[days-1];
+    for(int jx = 0; jx < days; jx++){
+        if (max == NULL || values[jx] > max){
+            if(values[jx] == max && jx == 1){
+                if(values[jx] - noSell > 0)
+                    max = values[jx];
+            }
+            else if (values[jx] == max && jx == )
+        } 
+
+
+    }
 }
 
-int Wine::tradeWine(Wine values){
-    
+
+int main(){
+    int days = 0;
+    cout << "Enter the number of days: " << endl;
+    cin >> days;
+    int* values = new int[days];
+
+    cout << "Enter the values for each day: " << endl;
+    for (int ix = 0; ix < days; ix++){
+        cin >> values[ix];
+    }
+
 }
