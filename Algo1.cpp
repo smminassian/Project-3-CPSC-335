83a6900020b4ab613629acc7f7d6803d4981da2a
Sebastien Minassian <143156014+smminassian@users.noreply.github.com>
2025-04-23 12:39:13 -0700

    code works just need to make comments for both algos and add a few tweaks and we should be good

--- Algo1.cpp
+++ Algo1.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int maxCharm(int *Warr, int *itemArr, int n, int w){
-    int *dp = new int[w + 1];
-    for (int kx = 0; kx <= w+1; kx++)
+void maxCharm(int *Warr, float *itemArr, int n, int w)
+{
+    float *dp = new float[w];
+    for (int kx = 0; kx <= w + 1; kx++)
     {
         dp[kx] = 0;
     }
-    for (int ix = 0; ix )
+    for (int ix = 1; ix <= w; ix++)
+    {
+        for (int jx = 0; jx < n; jx++)
+        {
+            if (Warr[jx] <= ix)
+            {
+                dp[ix] = max(dp[ix], dp[ix - Warr[jx]] + itemArr[jx]);
+            }
+        }
+    }
+    cout << "Output is " << dp[w] << endl;
 
+    delete[] dp;
 }
 
-
-
-int main(){
+int main()
+{
     int n = 0;
     int W = 0;
     cout << "Enter number of bead types: " << endl;
@@ -22,12 +34,12 @@ int main(){
     cin >> W;
 
     int *WArr = new int[n];
-    int *itemArr = new int[n];
+    float *itemArr = new float[n];
 
     int val = 0;
 
     cout << "Enter the weights of each bead type: " << endl;
-    for (int ix = 0; ix < W; ix++)
+    for (int ix = 0; ix < n; ix++)
     {
         cin >> val;
         WArr[ix] = val;
@@ -40,4 +52,10 @@ int main(){
         itemArr[jx] = val2;
     }
 
+    maxCharm(WArr, itemArr, n, W);
+
+    delete[] WArr;
+    delete[] itemArr;
+
+    return 0;
 }
